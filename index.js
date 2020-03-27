b5823bc709d4a6e2e3fe3e5c7aa344c90217bebf
joyobo <joeyyeokl@gmail.com>
2020-03-27 16:48:04 +0800

    moving to this repo

---
+++ index.js
@@ -0,0 +1,177 @@
+"use strict";
+const logger    = require('./app//modules/logger');
+const push = require("./app/modules/database_mod");
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Load the SDK
+let RainbowSDK = require("rainbow-node-sdk");
+
+app.use('/public', express.static('public'));
+app.use('/static', express.static('static'));
+
+// Define your configuration
+let options = {
+    rainbow: {
+        host: "sandbox"
+    },
+    credentials: {
+        login: "joey_yeo@mymail.sutd.edu.sg", // To replace by your developer credendials
+        password: "OFLl[8d(Py~8" // To replace by your developer credentials
+    },
+    // Application identifier
+    application: {
+        appID: "b6f834105aed11eabf7e77d14e87b936",
+        appSecret: "LzUG5l0iM9YproZTONXSkwnRmeAl7cEWrxSyg3ziSHlPpOVGVA8YY5lC2R6B0IwT"
+    },
+
+    // Logs options
+    logs: {
+        enableConsoleLogs: true,
+        enableFileLogs: false,
+        "color": true,
+        "level": 'debug',
+        "customLabel": "vincent01",
+        "system-dev": {
+            "internals": false,
+            "http": false,
+        }, 
+        file: {
+            path: "/var/tmp/rainbowsdk/",
+            customFileName: "R-SDK-Node-Sample2",
+            level: "debug",
+            zippedArchive : false/*,
+            maxSize : '10m',
+            maxFiles : 10 // */
+        }
+    },
+    // IM options
+    im: {
+        sendReadReceipt: true
+    }
+};
+
+// Instantiate the SDK
+let rainbowSDK = new RainbowSDK(options);
+
+// Start the SDK
+rainbowSDK.start().then(() => {
+     // Do something when the SDK is connected to Rainbow
+
+     // Set static folders
+     // Grant access permission
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+
+    // get root html
+    app.get('/',function(req,res){
+    res.sendfile("./public/index.html");
+    });
+
+    // get all other htmls
+    /*
+    contactUs
+    chat
+    email
+    call
+    */
+    app.get('/chat.html', function(req, res){
+        res.sendfile("./public/chat.html");
+    })
+
+    app.get('/contactUs.html', function(req, res){
+        res.sendfile("./public/contactUs.html");
+    })
+
+    app.get('/email.html', function(req, res){
+        res.sendfile("./public/email.html");
+    })
+
+    app.get('/call.html', function(req, res){
+        res.sendfile("./public/call.html");
+    })
+
+    app.get('/vendors-sdk.min.js', function(req, res){
+        res.sendfile("./public/vendors-sdk.min.js");
+    })
+
+    app.get('/rainbow-sdk.min.js', function(req, res){
+        res.sendfile("./public/rainbow-sdk.min.js");
+    })
+
+    app.get('/chat.js', function(req, res){
+        res.sendfile("./public/chat.js");
+    })
+
+    app.post('/endChat', function(req, res){
+        var rbwbubbleid = req.body.bubbleid;
+        // set engage of the agent in the bubble from 1 to 0
+        console.log("bubbleid: "+rbwbubbleid);
+        res.end();
+    });
+    app.post('/guestLogin', async function(req, res){
+        var cat = req.body.cat;
+        var catArray = cat.split(',');
+        console.log("category: "+cat);
+        var category = catArray[0];
+        var skill = catArray[1];
+        console.log("category: "+category+"   skill: "+skill);
+        console.log("Creation of guest account request received.");
+        // Create account
+        var guestaccount = await rainbowSDK.admin.createAnonymousGuestUser(7200).then( (guest) => {
+            return guest;
+        }).catch((err) => {
+            logger.log("debug", "error creating user");
+        });
+        var contact_id = await rainbowSDK.contacts.getContactById(guestaccount.id);
+
+        // Create Bubble of name support
+        let withHistory = false;
+        var bubbleId;
+        var bubble = await rainbowSDK.bubbles.createBubble("Debug", "A little description of my bubble", withHistory).then((bubble) => {
+            bubbleId = bubble.id;
+            return bubble;
+        }).catch(function(err) {
+            console.log("Error creating bubble");
+        });
+
+        // Add guest into bubble
+        rainbowSDK.bubbles.inviteContactToBubble(contact_id, bubble, false, false, "").then(function(bubbleUpdated) {
+            // do something with the invite sent
+            logger.log("debug", "guest user has been added to bubble");
+            logger.log("debug", "bubble jid: "+ bubbleUpdated.jid);
+        }).catch(function(err) {
+            // do something if the invitation failed (eg. bad reference to a buble)
+            logger.log("debug", "guest user invite failed");
+        });
+
+        // Add agent into bubble
+        // Test function only
+        var agent_id = await rainbowSDK.contacts.getContactById("5e60e5ddd8084c29e64eba90");
+        rainbowSDK.bubbles.inviteContactToBubble(agent_id, bubble, false, false, "").then(function(bubbleUpdated) {
+            logger.log("debug", "agent added into bubble");                
+        }).catch(function(err) {
+            // do something if the invitation failed (eg. bad reference to a buble)
+            logger.log("debug", "agent user invite failed");
+        });
+    
+       
+
+        var loginCred = {"Username": guestaccount.loginEmail, "Password": guestaccount.password, "BubbleId": bubbleId};
+        
+        // returns the credentials for guest user account
+        res.end(JSON.stringify(loginCred));
+        
+    })
+     
+    var server = app.listen(8081, function () {
+        var host = server.address().address
+        var port = server.address().port
+        console.log("Example app listening at http://%s:%s", host, port)
+     });
+});
---
+++ public/chat.js
@@ -0,0 +1,130 @@
+$(function() {
+    console.log("Application started");
+
+    // Update the variables below with your applicationID and applicationSecret strings
+    var applicationID = "b6f834105aed11eabf7e77d14e87b936",
+        applicationSecret = "LzUG5l0iM9YproZTONXSkwnRmeAl7cEWrxSyg3ziSHlPpOVGVA8YY5lC2R6B0IwT";
+
+    var RainbowUsername, RainbowPassword, RainbowBubbleId;
+
+    // Define input stuff
+    var input = document.getElementById("submission");
+
+    // Define two buttons
+    var submitButton = document.getElementById('submit');
+    var endChatButton = document.getElementById('endChat');
+    var status = document.getElementById('status');
+
+    // Button event listener
+    submitButton.addEventListener("click", function(){
+        var htmldata = "<div class=\"ms-Grid-row\"><p style=\"margin-left: 20px; margin-right: 20px; margin-top:10px; padding:8px; background-color: #efefef; text-align: left;\"><i class=\"ms-Icon ms-Icon--SkypeMessage\" aria-hidden=\"true\"></i>" + input.value + "</p></div>";
+        var container = document.getElementById('sentMessages');
+        container.insertAdjacentHTML('beforeend', htmldata);
+        var bubble = rainbowSDK.bubbles.getBubbleById(RainbowBubbleId);
+        /*
+        .then(function(object){
+            console.log("bubble retrieved,", object);
+            return object;
+        }).catch(function(err){
+            console.log("Bubble retrieve failed.");
+        });
+        */
+        rainbowSDK.im.sendMessageToBubble(bubble, input.value);
+        input.value="";
+        // Add info to your own table
+        /*
+        var htmldata = "<div class=\"ms-Grid-row\"><p style=\"margin-left: 20px; margin-right: 20px; margin-top:10px; padding:8px; background-color: #efefef; text-align: left;\"><i class=\"ms-Icon ms-Icon--SkypeMessage\" aria-hidden=\"true\"></i>" + input.value + "</p></div>";
+        var container = document.getElementById('sentMessages');
+        container.insertAdjacentHTML('beforeend', htmldata);
+        */
+    });
+
+    endChatButton.addEventListener("click", function(){
+        var data={"bubbleid": RainbowBubbleId};
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('POST', 'endChat', true);
+        xhttp.setRequestHeader('Content-Type','application/json');
+        xhttp.send(JSON.stringify(data));
+    });
+
+    // Execute a function when the user releases a key on the keyboard
+    input.addEventListener("keyup", function(event) {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            submitButton.click();
+        }
+    });
+
+    /* Bootstrap the SDK */
+    angular.bootstrap(document, ["sdk"]).get("rainbowSDK");
+
+    /* Callback for handling the event 'RAINBOW_ONREADY' */
+    var onReady = function onReady() {
+        // Use of ajax to fetch result
+        console.log("Fetching result");
+         var data=localStorage.getItem('category');
+        var dataTosend={"cat": data};
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('POST', 'guestLogin', true);
+        xhttp.setRequestHeader('Content-Type','application/json');
+        // send the data over to server
+        xhttp.send(JSON.stringify(dataTosend));
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var result = JSON.parse(this.responseText);
+                RainbowUsername = result.Username;
+                RainbowPassword = result.Password;
+                RainbowBubbleId = result.BubbleId;
+                console.log("Data fetched");
+                console.log("bubble id is:", RainbowBubbleId);
+                
+                // Sign in to rainbow
+                rainbowSDK.connection.signin(RainbowUsername, RainbowPassword)
+                               .then(function(object) {
+                                status.innerHTML = 'Agent connected';
+                                    console.log("User login successful", object);
+                                })
+                                .catch(function(err) {
+                                    console.log("User login failed", err);
+                                });
+                console.log("Bubble id is", RainbowBubbleId);
+            }else if(this.status != 200){
+                console.log("Wrong when fetching data from api");
+            }
+        };
+    }
+
+    /* Callback for handling the event 'RAINBOW_ONCONNECTIONSTATECHANGED' */
+    var onLoaded = function onLoaded() {
+        console.log("SDK Loading");
+
+        rainbowSDK
+            .initialize(applicationID, applicationSecret)
+            .then(function() {
+                console.log("initialized sdk");
+            })
+            .catch(function(err) {
+                console.log("initialization error", err);
+            });
+    };
+
+    // Im handler
+    var onNewMessageReceived = function onNewMessageReceived(event) {
+        console.log("I received new message");
+        var htmldata = "<div class=\"ms-Grid-row\"><p style=\"margin-left: 20px; margin-right: 20px; margin-top:10px; padding:8px; background-color: #efefef; text-align: left;\"><i class=\"ms-Icon ms-Icon--DelveAnalyticsLogo\" aria-hidden=\"true\"></i>" + event.detail.message.data + "</p></div>";
+        var container = document.getElementById('sentMessages');
+        container.insertAdjacentHTML('beforeend', htmldata);
+    }
+
+    // add event listener
+    document.addEventListener(rainbowSDK.im.RAINBOW_ONNEWIMMESSAGERECEIVED, onNewMessageReceived)
+    /* Listen to the SDK event RAINBOW_ONREADY */
+    document.addEventListener(rainbowSDK.RAINBOW_ONREADY, onReady)
+    /* Listen to the SDK event RAINBOW_ONLOADED */
+    document.addEventListener(rainbowSDK.RAINBOW_ONLOADED, onLoaded)
+    /* Load the SDK */
+    rainbowSDK.load();
+});
